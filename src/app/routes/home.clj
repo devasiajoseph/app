9df0bbfb32738c42070aaf7c3b67c544e635a98b
Devasia Joseph <devasiajoseph@gmail.com>
2013-06-19 22:24:10 +0530

    First commit

---
+++ src/app/routes/home.clj
@@ -0,0 +1,15 @@
+(ns app.routes.home
+  (:use compojure.core)
+  (:require [app.views.layout :as layout]
+            [app.util :as util]))
+
+(defn home-page []
+  (layout/render
+    "home.html" {:content (util/md->html "/md/docs.md")}))
+
+(defn about-page []
+  (layout/render "about.html"))
+
+(defroutes home-routes
+  (GET "/" [] (home-page))
+  (GET "/about" [] (about-page)))
---
+++ src/app/views/layout.clj
@@ -0,0 +1,10 @@
+(ns app.views.layout
+  (:use noir.request)
+  (:require [clabango.parser :as parser]))
+
+(def template-path "app/views/templates/")
+
+(defn render [template & [params]]
+  (parser/render-file (str template-path template)
+                      (assoc params :context (:context *request*))))
+
